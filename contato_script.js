745f103d89d3612a4b1e9135d9340c16fe78b0de
lucianomenezesjr <luciano.menezesjunior@aluno.senai.br>
2025-03-12 09:28:27 -0300

    Animando os botões

---
+++ contato_script.js
@@ -0,0 +1,15 @@
+// Get the all buttons elements
+const buttons = document.querySelectorAll('.redesSociaisIcon');
+
+// Loop through each button and add event listeners
+buttons.forEach(button => {
+    button.addEventListener('mouseover', function() {
+      button.style.width = '26px';
+      button.style.height = '26px';
+    });
+  
+    button.addEventListener('mouseout', function() {
+      button.style.width = '20px';
+      button.style.height = '20px';
+    });
+  });
